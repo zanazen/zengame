d3667297f67aca92d037fee24aaec842f75236d3
Rosana <rcathayde2012@gmail.com>
2022-11-03 14:11:49 -0300

    Finalizando

--- js/dom.js
+++ js/dom.js
@@ -1,16 +1,12 @@
 const match = new ZenGame()
-const btnDeck = document.getElementById("btnDeck")
-btnDeck.addEventListener("click", (event) => {
-event.preventDefault()
-match.renderDeck()
-settingUpGame()
+const CartaSelecionada = document.getElementById("CartaSelecionada")
+const mensag = document.getElementById("mensag")
+//const btnDeck = document.getElementById("btnDeck")
+const carta = document.querySelector(".deck-carta")
+// evento do click
+carta.addEventListener("click", () => {
+    let cartas = match.renderDeck()
+    console.log(cartas)
+    CartaSelecionada.src = cartas[0]
+    mensag.innerText = match.mensagem(cartas[0])
 })
-function settingUpGame() {
-    const allBackCards = document.querySelectorAll(".show")
-    allBackCards.forEach((backCard) => {
-    backCard.addEventListener("click", () => {
-        let card = backCard.previousElementSibling
-        console.log(card)
-    })    
-    });
-}
--- js/logica.js
+++ js/logica.js
@@ -1,16 +1,41 @@
 class ZenGame{
-    constructor(){
+   constructor(){
         this.deck = [
             "./img/buda.jpeg",
-            "./img/lakshimi.jpeg",
+            "./img/lakshmi.jpeg",
             "./img/ganesha.jpeg"
         ];
     }
-        renderDeck(){
-        let sortDeck = this.deck.sort(() => {
-            return Math.random() - 0.5
-        })
-        let boardGame = document.getElementById("board")
-        console.log(boardGame)
+    renderDeck(){
+    let sortDeck = this.deck.sort(() => {
+        return Math.random() - 0.5
+    })
+   //console.log(sortDeck)
+//    sortDeck.forEach((img) => {
+//         let image1 = document.createElement("img")
+//         image1.scr = "./img/buda.jpeg"
+//         image1.classList.add("show")
+//         //
+//         let image2 = document.createElement("img")
+//         image2.scr = "./img/lakshmi.jpeg"
+//         image2.classList.add("show")
+//         //
+//         let image3 = document.createElement("img")
+//         image3.scr = "./img/ganesha.jpeg"
+//         image3.classList.add("show")
+//         //     
+//     })
+    return sortDeck;
     }
+mensagem (ultdeck) {
+    if (ultdeck === "./img/buda.jpeg") {
+        return "Buda lhe diz: Começar aos poucos é normal: Buda ensinou que tudo começa devagar e que não é preciso ter pressa. Assim como uma jarra é preenchida aos poucos com água, a sabedoria humana também é alcançada aos poucos."
+    }
+    if (ultdeck === "./img/ganesha.jpeg") {
+        return "Ganesha lhe diz: Não percam a fé na sua capacidade de encontrar a solução para os seus problemas, de resolvê-los com maestria, no seu discernimentos para decidirem o que abandonar e o que levar nessa nova jornada em busca de si mesmos e de sua filiação Divina."
+    }
+    if (ultdeck === "./img/lakshmi.jpeg") {
+        return "Lakshmi lhe diz: Lakshmi, Deusa da Fortuna e Abundância, receba dela Saúde, Força, Abundância e Prosperidade para a sua vida."
+    }
+}
 }
